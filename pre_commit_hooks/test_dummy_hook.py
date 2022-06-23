fe71337e38e9c04a03cdd99f8ad8da013ee44250
jszafran <jszafran.pv@gmail.com>
2022-06-23 23:02:20 +0200

    Add parsing filenames.

--- pre_commit_hooks/test_dummy_hook.py
+++ pre_commit_hooks/test_dummy_hook.py
@@ -5,6 +5,7 @@ import sys
 def main() -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument("--message")
+    parser.add_argument("filesnames", nargs="*")
     args = parser.parse_args(sys.argv[1:])
     retval = 0
 
